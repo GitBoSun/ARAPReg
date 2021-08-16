66a07d30c33665990d1905f06213df48868951bb
xiangruhuang <xiangruhuang816@gmail.com>
2021-08-16 17:10:38 +0000

    hey

--- train_and_test_dfaust.sh
+++ train_and_test_dfaust.sh
@@ -1,19 +1,16 @@
 #!/usr/bin/env bash
-#GPUS=8
-#PORT=${PORT:-61816}
-#CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT main.py --launcher pytorch \
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python -W ignore main.py \
 --out_channels 32 32 32 64 \
 --ds_factors 2 2 2 2 \
 --exp_name 'arap' \
 --device_idx 0 \
 --batch_size 64 \
---epochs 300 \
+--epochs 405 \
 --lr 1e-4 \
 --test_lr 1e-3 \
 --test_decay_step 5 \
 --arap_weight 5e-4 \
---use_arap_epoch 150 \
+--use_arap_epoch 450 \
 --arap_eig_k 30 \
 --decay_step 3 \
 --latent_channels 72 \
@@ -24,4 +21,5 @@ CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python -W ignore main.py \
 --distributed \
 --alsotest \
 --continue_train True \
-#--checkpoint work_dir/DFaust/out/model0/checkpoints/checkpoint_0850.pt
+--checkpoint work_dir/DFaust/out/arap/checkpoints/checkpoint_0400.pt \
+--test_checkpoint work_dir/DFaust/out/arap/test_checkpoints/checkpoint_01200.pt \
