14ca88a2a34ce8b46200f5d14932ef7fde3ba771
Bo Sun <31983769+GitBoSun@users.noreply.github.com>
2021-09-11 00:42:38 -0500

    Update test_bone.sh

--- test_bone.sh
+++ test_bone.sh
@@ -2,18 +2,18 @@
 CUDA_VISIBLE_DEVICES=0 python main.py \
 --exp_name 'model0' \
 --device_idx 0 \
---epochs 2500 \
+--epochs 4500 \
 --lr 8e-3 \
---arap_weight 0.0001 \
+--arap_weight 0.05 \
 --use_arap_epoch 1500 \
 --decay_step 10 \
 --latent_channels 8 \
 --use_vert_pca True \
---work_dir ./work_dir/bone/femur \
+--work_dir ./work_dir/bone/tibia \
 --dataset Bone \
 --data_dir ./data/bone/tibia \
 --continue_train True \
 --test_lr 8e-2 \
---test_epochs 500 \
+--test_epochs 2500 \
 --test_decay_step 30 \
 --mode test \
